a4b57fc8294191dceea8fbb11ac57e9cac4fdef8
cdq <cdq@bupt.edu.cn>
2022-04-19 17:09:20 +0800

    find best hp setup

---
+++ experiments/distributed/transformer_exps/run_tc_exps/run_text_classification_freeze_setup.sh
@@ -0,0 +1,67 @@
+# cd /home/cdq/FedNLP/experiments/distributed/transformer_exps/run_tc_exps/ && conda activate fednlp && sh run_text_classification_freeze.sh FedAvg "uniform" 0.1 1 3000 5 e,0,1,2,3,4
+FL_ALG=$1
+PARTITION_METHOD=$2
+C_LR=$3
+S_LR=$4
+ROUND=$5
+WORKER_NUM=$6
+LAYERS=$7
+DEPTH=$8
+TIME=$9
+DATA=$10
+BS=$11
+
+LOG_FILE="fedavg_transformer_tc.log"
+# WORKER_NUM=10
+CI=0
+
+DATA_DIR=/data/cdq/fednlp_data/
+DATA_NAME=$DATA
+PROCESS_NUM=`expr $WORKER_NUM + 1`
+echo $PROCESS_NUM
+
+hostname > mpi_host_file
+
+mpirun -np $PROCESS_NUM -hostfile mpi_host_file \
+python -m fedavg_main_tc_baseline \
+  --gpu_mapping_file "gpu_mapping.yaml" \
+  --gpu_mapping_key cdq-${WORKER_NUM}-setup \
+  --client_num_per_round $WORKER_NUM \
+  --comm_round $ROUND \
+  --ci $CI \
+  --dataset "${DATA_NAME}" \
+  --data_file "${DATA_DIR}/data_files/${DATA_NAME}_data.h5" \
+  --partition_file "${DATA_DIR}/partition_files/${DATA_NAME}_partition.h5" \
+  --partition_method $PARTITION_METHOD \
+  --fl_algorithm $FL_ALG \
+  --model_type bert \
+  --model_name bert-base-uncased \
+  --do_lower_case True \
+  --train_batch_size $BS \
+  --eval_batch_size 8 \
+  --max_seq_length 256 \
+  --lr $C_LR \
+  --server_lr $S_LR \
+  --epochs 1 \
+  --output_dir "./tmp/${DATA_NAME}_fedavg_output_setup-${DEPTH}-${TIME}/" \
+  --freeze_layers $LAYERS 
+
+
+
+# sh run_text_classification.sh FedAvg "niid_label_clients=100_alpha=5.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedAvg "niid_label_clients=100_alpha=10.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedAvg "niid_label_clients=100_alpha=1.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedAvg "uniform" 5e-5 0.1 50
+# sh run_text_classification.sh FedAvg "niid_quantity_clients=100_beta=5.0" 5e-5 0.1 50
+
+# sh run_text_classification.sh FedProx "niid_label_clients=100_alpha=5.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedProx "niid_label_clients=100_alpha=10.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedProx "niid_label_clients=100_alpha=1.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedProx "uniform" 5e-5 0.1 50
+# sh run_text_classification.sh FedProx "niid_quantity_clients=100_beta=5.0" 5e-5 0.1 50
+
+# sh run_text_classification.sh FedOPT "niid_label_clients=100_alpha=5.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedOPT "niid_label_clients=100_alpha=10.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedOPT "niid_label_clients=100_alpha=1.0" 5e-5 0.1 50
+# sh run_text_classification.sh FedOPT "uniform" 5e-5 0.1 300
+# sh run_text_classification.sh FedOPT "niid_quantity_clients=100_beta=5.0" 5e-5 0.1 50
--- experiments/distributed/transformer_exps/run_tc_exps/sweep_tc_freeze_setup.py
+++ experiments/distributed/transformer_exps/run_tc_exps/sweep_tc_freeze_setup.py
@@ -13,7 +13,7 @@ def add_args(parser):
 
 
 def wait_for_the_training_process(args):
-    pipe_path = "./tmp/fedml-baseline-{args.dataset}".format(args=args)
+    pipe_path = "./tmp/fedml-setup-{args.dataset}".format(args=args)
     pipe_fd = os.open(pipe_path, os.O_RDONLY | os.O_NONBLOCK)
     with os.fdopen(pipe_fd) as pipe:
         while True:
@@ -34,7 +34,7 @@ def set_hp(delta_round, freeze_layers, args):
         if args.dataset == "20news":
             partition_method = "uniform"
 
-        hp = 'FedAvg ' + partition_method + ' 0.1 0.1 ' + str(delta_round) + ' 5 ' + remove_space(str([freeze_layers[2]]).replace(',','.')+','+str([-1]).replace(',','.')+','+str(freeze_layers[2]))+','+str(freeze_layers[3][-1]).replace(',','.')+" "+str(args.depth)+" "+str(args.time_threshold) +" "+str(args.dataset) + " shallow" # linux 读取输入的时候以，为分隔符，需要替换掉
+        hp = 'FedAvg ' + partition_method + ' 0.1 0.1 ' + str(delta_round) + ' ' + str(args.client_num) + ' ' + remove_space(str([freeze_layers[2]]).replace(',','.')+','+str([-1]).replace(',','.')+','+str(freeze_layers[2]))+','+str(freeze_layers[3][-1]).replace(',','.')+" "+str(args.depth)+" "+str(args.time_threshold) +" "+str(args.dataset) + " " + str(args.bs) # linux 读取输入的时候以，为分隔符，需要替换掉
 
         return hp
 
@@ -43,7 +43,7 @@ def remove_space(s):
     return s_no_space
 
 def remove_cache_model(args):
-    os.system("rm -rf .\/tmp\/{args.dataset}_fedavg_output_baseline-{args.depth}-{args.time_threshold}".format(args=args))
+    os.system("rm -rf .\/tmp\/{args.dataset}_fedavg_output_setup-{args.depth}-{args.time_threshold}".format(args=args))
 
 # customize the log format
 logging.basicConfig(level=logging.INFO,
@@ -115,45 +115,49 @@ hps_parallelism = [
 ]
 
 args.round = -1 
-args.depth = 0
-args.width = 8
+args.depth = 11
+args.width = 32
 args.time_threshold = 60
 args.max_round = 3000
+args.bs = 8
+args.client_num = 5
 
 args.dataset = "agnews" # "agnews", "20news", "semeval_2010_task8"
 
 # freeze_layers = [[depth],[round],depth,[width]] 
 width = [32, 40, 48, 56, 64]
 depth = [0,1,2,3,4,5,6]
+bs = [8,16,32]
+client_num = [5,10]
+datasets = ["agnews", "20news", "semeval_2010_task8"]
 freeze_layers = [[6],[-1],6,[width]] 
 
 remove_cache_model(args)
 
 run_id = 0
 
-for w in width:
-    for d in depth:
-        args.depth = d
-        args.width = w
-        freeze_layers = [[d],[-1],d,[w]] 
-        args.hp = set_hp(400, freeze_layers,args)
-        args.run_id = run_id
-        
-        logging.info("hp = %s" % args.hp)
-        # os.system("perl -p -i -e 's/pipe_path = .*/pipe_path = \".\/tmp\/fedml-baseline-{args.dataset}\"/g' /home/cdq/FedNLP/FedML/fedml_api/distributed/fedavg/utils.py".format(args=args)) # pipe tmp name
-        # logging.info("perl -p -i -e 's/pipe_path = .*/pipe_path = \".\/tmp\/fedml-baseline-{args.dataset}\"/g' /home/cdq/FedNLP/FedML/fedml_api/distributed/fedavg/utils.py".format(args=args))
+for dataset in datasets[::-1]:
+    for b in bs:
+        for c in client_num:
+            args.dataset = dataset
+            args.bs = b
+            args.client_num = c
+            freeze_layers = [[args.depth],[-1],args.depth,[args.width]] 
+            args.hp = set_hp(500, freeze_layers,args)
+            args.run_id = run_id
+            
+            logging.info("hp = %s" % args.hp)
+ 
+            os.system("mkdir ./tmp/; touch ./tmp/fedml-setup-{args.dataset}; mkdir ./results/BERT/{args.dataset}-setup".format(args=args))
+            logging.info('nohup sh run_text_classification_freeze_setup.sh '
+                    '{args.hp} '
+                    '> ./results/BERT/{args.dataset}-setup/fednlp_tc_dataset_{args.dataset}_batchsize_{args.bs}_client_num_{args.client_num}.log 2>&1 &'.format(args=args))
+            os.system('nohup sh run_text_classification_freeze_setup.sh '
+                    '{args.hp} '
+                    '> ./results/BERT/{args.dataset}-setup/fednlp_tc_dataset_{args.dataset}_batchsize_{args.bs}_client_num_{args.client_num}.log 2>&1 &'.format(args=args))
+            
 
-        
-        os.system("mkdir ./tmp/; touch ./tmp/fedml-baseline-{args.dataset}; mkdir ./results/BERT/{args.dataset}-baseline".format(args=args))
-        logging.info('nohup sh run_text_classification_freeze_baseline.sh '
-                '{args.hp} '
-                '> ./results/BERT/{args.dataset}-baseline/fednlp_tc_width_{args.width}_depth_{args.depth}.log 2>&1 &'.format(args=args))
-        os.system('nohup sh run_text_classification_freeze_baseline.sh '
-                '{args.hp} '
-                '> ./results/BERT/{args.dataset}-baseline/fednlp_tc_width_{args.width}_depth_{args.depth}.log 2>&1 &'.format(args=args))
-        
+            wait_for_the_training_process(args)
 
-        wait_for_the_training_process(args)
-
-        sleep(5)
-        run_id += 1
+            sleep(5)
+            run_id += 1
